e3e9657f8a6dfa402392e37a9740fe6c1996e2ad
melvin <melvintk09@gmail.com>
2024-01-10 14:58:04 +0530

    display all messages in the array

--- routes/index.js
+++ routes/index.js
@@ -19,24 +19,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
-// Get form page
-router.get('/new', (req,res)=>{
-  res.render('form'); //to render the form template
-});
 
-// Import body-parser middleware
-const bodyParser = require('body-parser');
 
-// Use body-parser middleware to parse form data
-router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/new', (req, res) => {
-  const messageText = req.body.message;
-  const messageUser = req.body.author;
-
-  messages.push({ text: messageText, user: messageUser, added: new Date() });
-
-  res.redirect('/'); // Redirect to the index page
-});
 
 module.exports = router;
